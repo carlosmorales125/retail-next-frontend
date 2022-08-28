12d5d57e3db26a536563320e01db802e8be1cb13
Carlos Morales <carlos.rey.morales@gmail.com>
2022-08-28 17:32:28 -0400

    feature/addedGetUserApiCallAndUsersComponent

---
+++ src/api/users.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const getUsers = async () => {
+    try {
+        const response = await axios.get('http://localhost:3000/api/v1/users');
+        if (response?.data?.users) {
+            return response.data.users;
+        } else {
+            throw new Error('call to http://localhost:3000/api/v1/users has failed');
+        }
+    } catch (e) {
+        console.log(e);
+    }
+};
