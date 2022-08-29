6c3a7929b045bcf7a0b01daf1f3fc5fba05e9c31
Carlos Morales <carlos.rey.morales@gmail.com>
2022-08-28 22:39:45 -0400

    feature/changedApiPort

--- src/api/users.js
+++ src/api/users.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 export const getUsers = async () => {
     try {
-        const response = await axios.get('http://localhost:3000/api/v1/users');
+        const response = await axios.get('http://localhost:8000/api/v1/users');
         if (response?.data?.users) {
             return response.data.users;
         } else {
-            throw new Error('call to http://localhost:3000/api/v1/users has failed');
+            throw new Error('call to http://localhost:8000/api/v1/users has failed');
         }
     } catch (e) {
         console.log(e);
